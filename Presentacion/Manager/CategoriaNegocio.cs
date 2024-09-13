3fec75fa394864874d748adc34696a73c9d7ca3e
JesicaValdez <jesica.valddez@gmail.com>
2024-09-13 02:23:28 +0100

    Primer commit
    Añadimos la arquitectura en capas con los proyectos Dominio y Manager.
    Agrego las clases:
    - AccesoDB
    - Categoria
    - CategoriaNegocio

---
+++ Presentacion/Manager/CategoriaNegocio.cs
@@ -0,0 +1,61 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class CategoriaNegocio
+    {
+        AccesoDB datos = new AccesoDB();
+
+        public List<Categoria> ListarCategorias()
+        {
+            List<Categoria> lista = new List<Categoria>();
+
+            try
+            {
+                datos.setearConsulta("SELECT * FROM CATEGORIAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+
+        public void agregarCategoria(Categoria nueva)
+        {
+            try
+            {
+                datos.setearConsulta("insert into CATEGORIAS VALUES ('" + nueva.Descripcion + "')");
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally { datos.cerrarConexion(); }
+        }
+
+    }
+}
